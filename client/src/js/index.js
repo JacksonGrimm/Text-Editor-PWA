96d2424c9fe9cde821f1563e306fe957120a1004
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2023-01-07 15:24:19 -0700

    added icon image to dist thought webpack and index.js

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,33 +1,38 @@
-import { Workbox } from 'workbox-window';
-import Editor from './editor';
-import './database';
-import '../css/style.css';
+import { Workbox } from "workbox-window";
+import Editor from "./editor";
+import "./database";
+import "../css/style.css";
+import Icon from "../images/logo.png";
 
-const main = document.querySelector('#main');
-main.innerHTML = '';
+const main = document.querySelector("#main");
+main.innerHTML = "";
 
 const loadSpinner = () => {
-  const spinner = document.createElement('div');
-  spinner.classList.add('spinner');
+  const spinner = document.createElement("div");
+  spinner.classList.add("spinner");
   spinner.innerHTML = `
   <div class="loading-container">
   <div class="loading-spinner" />
   </div>
   `;
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  main.appendChild(myIcon);
   main.appendChild(spinner);
 };
 
 const editor = new Editor();
 
-if (typeof editor === 'undefined') {
+if (typeof editor === "undefined") {
   loadSpinner();
 }
 
 // Check if service workers are supported
-if ('serviceWorker' in navigator) {
+if ("serviceWorker" in navigator) {
   // register workbox service worker
-  const workboxSW = new Workbox('/src-sw.js');
+  const workboxSW = new Workbox("/src-sw.js");
   workboxSW.register();
 } else {
-  console.error('Service workers are not supported in this browser.');
+  console.error("Service workers are not supported in this browser.");
 }
